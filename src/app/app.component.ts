4763bf81d7239f95175ee252c602ed23c3cf25c1
Rai Ron Carl Miranda <rai.miranda@ibm.com>
2025-10-21 15:01:58 +0800

    Change from Sing to Phil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app Singapore Region';
+  title = 'my-app Philippines Region';
 }
